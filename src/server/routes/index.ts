9ef10848851dae0710d591324d7f0ded7c6a82a7
João Lessa <joaovictorlessa98@gmail.com>
2023-09-21 14:31:48 -0300

    Aula 9 e 10

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,17 +1,15 @@
 import { Router } from 'express';
 import {StatusCodes} from 'http-status-codes';
+import {CidadesController} from './../controllers';
+
+
 const router = Router();
 
 router.get('/', (req, res)=>{
     return res.send('Olá, dev!');
 });
 
-router.post('/teste', (req, res)=>{
-    console.log(req);
-
-    return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
-});
-
+router.post('/cidades', CidadesController.create);
 
 
 export {router};
